4bfbf337f88ed6d79e5d2800e726630436fe86c6
Fabio Martins <fabio@inventae.com.br>
2014-10-19 16:05:09 -0200

    Segment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019180125) do
+ActiveRecord::Schema.define(version: 20141019180438) do
 
   create_table "addresses", force: true do |t|
     t.string   "street_suffix"
@@ -86,6 +86,12 @@ ActiveRecord::Schema.define(version: 20141019180125) do
   end
 
   add_index "contact_phones", ["address_id"], name: "index_contact_phones_on_address_id", using: :btree
+
+  create_table "segments", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "states", force: true do |t|
     t.string   "name"
