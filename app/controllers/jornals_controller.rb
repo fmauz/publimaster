231e180a39f55a60d0a73f6a88a0fa41a45825c9
Fabio Martins <fabio@inventae.com.br>
2014-12-02 11:36:58 -0200

    Criando jornais

--- app/controllers/jornals_controller.rb
+++ app/controllers/jornals_controller.rb
@@ -4,4 +4,17 @@ class JornalsController < ApplicationController
     set_header_pagination( @jornals )
     render json: @jornals  
   end
+
+  def create
+    @jornal = Jornal.new( jornal_params )
+    if @jornal.save
+      render json: @jornal, status: 200
+    else
+      render json: @jornal.errors, status: 422
+    end
+  end
+
+  def jornal_params
+    params.require( :jornal ).permit( :name, :contact, :observation, :state_id )
+  end
 end
--- app/models/jornal.rb
+++ app/models/jornal.rb
@@ -1,3 +1,7 @@
 class Jornal < ActiveRecord::Base
   belongs_to :state
+
+  validates :name,
+            :state,
+            presence: true
 end
